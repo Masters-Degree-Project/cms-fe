552bd8760e6537af409c65c401e1222056eb321b
migokcek <ilyasgkck@gmail.com>
2025-01-12 19:47:23 +0100

    loading zIndex fixed

--- src/components/loading/Loading.tsx
+++ src/components/loading/Loading.tsx
@@ -1,4 +1,4 @@
-'use client';
+'use client'
 
 import React from 'react'
 
@@ -12,20 +12,22 @@ function Loading({}: Props) {
   const { loading } = useLoading()
 
   return (
-    <div style={{
-      position: 'fixed',
-      left: 0,
-      top: 0,
-      width: '100vw',
-      height: '100%',
-      backgroundColor: 'rgba(0,0,0,0.2)',
-      zIndex: loading ? 1000000 : -1000000,
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      opacity: loading ? 1 : 0,
-      transition: 'all 0.6s ease-in-out',
-    }} >
+    <div
+      style={{
+        position: 'fixed',
+        left: 0,
+        top: 0,
+        width: '100vw',
+        height: '100%',
+        backgroundColor: 'rgba(0,0,0,0.2)',
+        zIndex: loading ? 999999999 : -1000000,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        opacity: loading ? 1 : 0,
+        transition: 'all 0.6s ease-in-out'
+      }}
+    >
       <CircularProgress />
     </div>
   )
